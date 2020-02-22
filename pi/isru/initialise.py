eb9a17ac79c5207767645cd9e06a542f6497abdc
GeorgieChallis <g.l.challis@student.reading.ac.uk>
2020-02-22 14:21:11 +0000

    Blocking Arduino init function moved to thread

--- pi/isru/initialise.py
+++ pi/isru/initialise.py
@@ -1,31 +1,47 @@
 import serial
 import time
+import threading
 
-connectionOK = False
+# Initialise Arduino Comms
 
-mySerial = serial.Serial('/dev/ttyACM0', timeout = 1)
-mySerial.baudrate = 115200
-
-time.sleep(1)
-
+arduino_ok = False
 data = ""
-prevData = ""
-
-i = 0
-
-#Send hello to initiate connection (try 5 times)
-while i < 5:
-    msg =  'hello!'
-    #print(msg.encode('utf-8'))
-    mySerial.write(msg.encode('utf-8'))
-    i +=  1
-
-while mySerial.inWaiting():
-    x = mySerial.read().decode("utf-8") # decode needed for Python3
-    if x != '!':
-        data += x
-    else:
-        print(data)
 
 
-mySerial.close()
+def init_arduino():
+    global arduino_ok
+    global data
+    my_serial = serial.Serial('/dev/ttyACM0', timeout=1)
+    my_serial.baudrate = 115200
+
+    time.sleep(1)
+
+    prev_data = ""
+
+    i = 0
+
+    # Send hello! to initiate connection (try 5 times)
+    while i < 5:
+        if not arduino_ok:
+            msg = 'hello!'
+            my_serial.write(msg.encode('utf-8'))
+        i += 1
+
+    # Read back from Serial to look for the !
+    while my_serial.inWaiting():
+        x = my_serial.read().decode("utf-8") # decode needed for Python3
+        if x != '!':
+            data += x
+        else:
+            print(data)
+            data = ""
+            arduino_ok = True
+
+arduino_thread = threading.Thread(target=init_arduino)
+
+arduino_thread.start()
+
+# do everything else we need to do to initialise
+
+
+arduino_thread.join()
