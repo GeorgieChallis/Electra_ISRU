3b9deaeffe7d50ffafcdde7c088b8d49c11d0f5e
prozzenn <prozzenn@gmail.com>
2020-03-04 17:14:57 +0000

    new client file - comment to keep georgie happy

---
+++ pi/client.py
@@ -0,0 +1,15 @@
+import socket
+HOST = "192.168.43.50"
+PORT = 9000
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+while True:
+    command = input ("enter command: ")
+    s.send(str(command).encode('utf-8'))
+    reply = s.recv(1024).decode('utf-8')
+    if reply == 'Terminating':
+        break
+    print (reply)
+s.close()
+exit()
